1f4661ba73b5bdd61f0edfedbec3fd06cd1295a5
Vivek <142565389+vivek7941@users.noreply.github.com>
2026-03-27 11:53:45 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://digitalheroes-bkdq.onrender.com/"
+  baseURL: "https://digitalheroes-bkdq.onrender.com"
 });
 
 export default API;
